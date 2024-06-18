216aad66d75ee3e13d530513adaeb5fad45cf6c7
Joel <dombekj890@gmail.com>
2024-06-17 20:06:54 -0400

    updates

---
+++ collapse.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('.toggle-link');
+    var allLists = document.querySelectorAll('ul');
+    var toggleAllLink = document.getElementById('toggle-all-link');
+
+    function createSvgIcon(direction) {
+        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('focusable', 'false');
+        svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
+        svg.setAttribute('xmlns', 'https://www.w3.org/2000/svg');
+        svg.setAttribute('aria-hidden', 'true');
+        svg.setAttribute('viewBox', '0 0 20 20');
+        svg.setAttribute('class', 'svg-icon ' + direction + '-icon');
+
+        var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        if (direction === 'collapsed') {
+            path.setAttribute('d', 'M11.8 2.8L10.8 3.8 16.2 9.3 1 9.3 1 10.7 16.2 10.7 10.8 16.2 11.8 17.2 19 10z');
+        } else {
+            path.setAttribute('d', 'M8.2 17.2L9.2 16.2 3.8 10.7 19 10.7 19 9.3 3.8 9.3 9.2 3.8 8.2 2.8 1 10z');
+        }
+        svg.appendChild(path);
+        return svg;
+    }
+
+    links.forEach(function(link) {
+        var targetId = link.getAttribute('data-target');
+        var targetList = document.getElementById(targetId);
+
+        var collapsedIcon = createSvgIcon('collapsed');
+        var expandedIcon = createSvgIcon('expanded');
+        expandedIcon.style.display = 'none';
+
+        link.appendChild(collapsedIcon);
+        link.appendChild(expandedIcon);
+
+        // Set initial link text based on initial state
+        if (targetList.classList.contains('collapse')) {
+            link.firstChild.nodeValue = 'Show';
+        } else {
+            link.firstChild.nodeValue = 'Hide';
+            collapsedIcon.style.display = 'none';
+            expandedIcon.style.display = 'inline';
+        }
+
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            targetList.classList.toggle('collapse');
+
+            // Update link text and icon based on the state
+            if (targetList.classList.contains('collapse')) {
+                this.firstChild.nodeValue = 'Show';
+                collapsedIcon.style.display = 'inline';
+                expandedIcon.style.display = 'none';
+            } else {
+                this.firstChild.nodeValue = 'Hide';
+                collapsedIcon.style.display = 'none';
+                expandedIcon.style.display = 'inline';
+            }
+        });
+    });
+
+    toggleAllLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        var allCollapsed = Array.from(allLists).every(list => list.classList.contains('collapse'));
+        allLists.forEach(function(list) {
+            if (allCollapsed) {
+                list.classList.remove('collapse');
+            } else {
+                list.classList.add('collapse');
+            }
+        });
+        links.forEach(function(link) {
+            var targetId = link.getAttribute('data-target');
+            var targetList = document.getElementById(targetId);
+            var collapsedIcon = link.querySelector('.collapsed-icon');
+            var expandedIcon = link.querySelector('.expanded-icon');
+            if (targetList.classList.contains('collapse')) {
+                link.firstChild.nodeValue = 'Show';
+                collapsedIcon.style.display = 'inline';
+                expandedIcon.style.display = 'none';
+            } else {
+                link.firstChild.nodeValue = 'Hide';
+                collapsedIcon.style.display = 'none';
+                expandedIcon.style.display = 'inline';
+            }
+        });
+        this.firstChild.nodeValue = allCollapsed ? 'Hide All' : 'Show All';
+    });
+});
--- script.js
+++ script.js
@@ -1,118 +1,227 @@
 const resolver = {
-  resolve: function resolve(options, callback) {
-    // The string to resolve
-    const resolveString = options.resolveString || options.element.getAttribute('data-target-resolver');
-    const combinedOptions = Object.assign({}, options, {resolveString: resolveString});
+    resolve: function resolve(options, callback) {
+        // The string to resolve
+        const resolveString =
+            options.resolveString ||
+            options.element.getAttribute("data-target-resolver");
+        const combinedOptions = Object.assign({}, options, {
+            resolveString: resolveString
+        });
 
-    function getRandomInteger(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-
-    function randomCharacter(characters) {
-      return characters[getRandomInteger(0, characters.length - 1)];
-    };
-
-    function doRandomiserEffect(options, callback) {
-      const characters = options.characters;
-      const timeout = options.timeout;
-      const element = options.element;
-      const partialString = options.partialString;
-
-      let iterations = options.iterations;
-
-      setTimeout(() => {
-        if (iterations >= 0) {
-          const nextOptions = Object.assign({}, options, {iterations: iterations - 1});
-
-          // Ensures partialString without the random character as the final state.
-          if (iterations === 0) {
-            element.textContent = partialString;
-          } else {
-            // Replaces the last character of partialString with a random character
-            element.textContent = partialString.substring(0, partialString.length - 1) + randomCharacter(characters);
-          }
-
-          doRandomiserEffect(nextOptions, callback)
-        } else if (typeof callback === "function") {
-          callback();
+        function getRandomInteger(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-      }, options.timeout);
-    };
 
-    function doResolverEffect(options, callback) {
-      const resolveString = options.resolveString;
-      const characters = options.characters;
-      const offset = options.offset;
-      const partialString = resolveString.substring(0, offset);
-      const combinedOptions = Object.assign({}, options, {partialString: partialString});
-
-      doRandomiserEffect(combinedOptions, () => {
-        const nextOptions = Object.assign({}, options, {offset: offset + 1});
-
-        if (offset <= resolveString.length) {
-          doResolverEffect(nextOptions, callback);
-        } else if (typeof callback === "function") {
-          callback();
+        function randomCharacter(characters) {
+            return characters[getRandomInteger(0, characters.length - 1)];
         }
-      });
-    };
 
-    doResolverEffect(combinedOptions, callback);
-  }
-}
+        function doRandomiserEffect(options, callback) {
+            const characters = options.characters;
+            const timeout = options.timeout;
+            const element = options.element;
+            const partialString = options.partialString;
+
+            let iterations = options.iterations;
+
+            setTimeout(() => {
+                if (iterations >= 0) {
+                    const nextOptions = Object.assign({}, options, {
+                        iterations: iterations - 1
+                    });
+
+                    // Ensures partialString without the random character as the final state.
+                    if (iterations === 0) {
+                        element.textContent = partialString;
+                    } else {
+                        // Replaces the last character of partialString with a random character
+                        element.textContent =
+                            partialString.substring(0, partialString.length - 1) +
+                            randomCharacter(characters);
+                    }
+
+                    doRandomiserEffect(nextOptions, callback);
+                } else if (typeof callback === "function") {
+                    callback();
+                }
+            }, options.timeout);
+        }
+
+        function doResolverEffect(options, callback) {
+            const resolveString = options.resolveString;
+            const characters = options.characters;
+            const offset = options.offset;
+            const partialString = resolveString.substring(0, offset);
+            const combinedOptions = Object.assign({}, options, {
+                partialString: partialString
+            });
+
+            doRandomiserEffect(combinedOptions, () => {
+                const nextOptions = Object.assign({}, options, { offset: offset + 1 });
+
+                if (offset <= resolveString.length) {
+                    doResolverEffect(nextOptions, callback);
+                } else if (typeof callback === "function") {
+                    callback();
+                }
+            });
+        }
+
+        doResolverEffect(combinedOptions, callback);
+    }
+};
+
+// const strings = [
+//   'BRANDING',
+//   'UX',
+//   'UI',
+//   'FRONT-END',
+//   'USER-EXPERIENCE',
+//   'USER-INTERFACE',
+//   '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
+//   'BEGIN WITH THE END IN MIND',
+//   'DESIGN IS HOW IT WORKS.',
+//   '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
+//   'HELL0 WORLD!',
+//   'TEST. TEST. TEST.',
+//   '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
+//   'GET RID OF MORE.',
+//   'DO NOT EVER MAKE IT "POP".',
+//   '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
+//   'SPEAK TO GOALS, NOT FEATURES.',
+//   'YOUR GOALS DON\'T CARE HOW YOU FEEL.',
+//   '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
+//   '( ͡° ͜ʖ ͡°)',
+// ];
 
 const strings = [
-  'BRANDING',
-  'UX',
-  'UI',
-  'FRONT-END',
-  'USER-EXPERIENCE',
-  'USER-INTERFACE',
-  '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
-  'BEGIN WITH THE END IN MIND',
-  'DESIGN IS HOW IT WORKS.',
-  '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
-  'HELL0 WORLD!',
-  'TEST. TEST. TEST.',
-  '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
-  'GET RID OF MORE.',
-  'DO NOT EVER MAKE IT "POP".',
-  '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
-  'SPEAK TO GOALS, NOT FEATURES.',
-  'YOUR GOALS DON\'T CARE HOW YOU FEEL.',
-  '▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂▂▂▂▃▅▇█▓▒░░▒▓█▇▅▃▂',
-  '( ͡° ͜ʖ ͡°)',
+    "BRANDING",
+    "UX",
+    "UI",
+    "FRONT_END",
+    "USER_EXPERIENCE",
+    "USER_INTERFACE",
+    "BEGIN WITH THE END IN MIND",
+    "DESIGN IS HOW IT WORKS.",
+    "HELL0 WORLD!",
+    "TEST. TEST. TEST.",
+    "GET RID OF MORE.",
+    'DO NOT EVER MAKE IT "POP".',
+    "SPEAK TO GOALS, NOT FEATURES.",
+    "YOUR GOALS DON'T CARE HOW YOU FEEL."
 ];
 
 let counter = 0;
 
 const options = {
-  // Initial position
-  offset: 0,
-  // Timeout between each random character
-  timeout: 10,
-  // Number of random characters to show
-  iterations: 5,
-  // Random characters to pick from
-  characters: ['$', '3', '8', 'd', '_', 'f', 'g', '!', '5', 'j', 'k', 'l', '2', 'n', '_', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='],
-  // String to resolve
-  resolveString: strings[counter],
-  // The element
-  element: document.querySelector('[data-target-resolver]'),
-}
+    // Initial position
+    offset: 0,
+    // Timeout between each random character
+    timeout: 10,
+    // Number of random characters to show
+    iterations: 5,
+    // Random characters to pick from
+    characters: [
+        "$",
+        "3",
+        "8",
+        "d",
+        "_",
+        "f",
+        "g",
+        "!",
+        "5",
+        "j",
+        "k",
+        "l",
+        "2",
+        "n",
+        "_",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "x",
+        "y",
+        "x",
+        "#",
+        "%",
+        "&",
+        "-",
+        "+",
+        "_",
+        "?",
+        "/",
+        "\\",
+        "="
+    ],
+    // String to resolve
+    resolveString: strings[counter],
+    // The element
+    element: document.querySelector("[data-string-01]")
+};
 
 // Callback function when resolve completes
 function callback() {
-  setTimeout(() => {
-    counter ++;
+    setTimeout(() => {
+        counter++;
 
-    if (counter >= strings.length) {
-      counter = 0;
-    }
+        if (counter >= strings.length) {
+            counter = 0;
+        }
 
-    let nextOptions = Object.assign({}, options, {resolveString: strings[counter]});
-    resolver.resolve(nextOptions, callback);
-  }, 1000);
+        let nextOptions = Object.assign({}, options, {
+            resolveString: strings[counter]
+        });
+        resolver.resolve(nextOptions, callback);
+    }, 1000);
 }
 
 resolver.resolve(options, callback);
+
+//right click
+// if (document.addEventListener) {
+//     // IE >= 9; other browsers
+//     document.addEventListener(
+//         "contextmenu",
+//         function(e) {
+//             alert(
+//                 "You've tried to open context menu by right clicking.\n\nI've disabled this to make it a smidge more difficult to see behind the scenes of the code and content I have spent a ton of time currating that defines my career here as well as to safegaurd my personal information.\n\n\nThere are others ways to see the source if you really need to ;}"
+//             ); //here you draw your own menu
+//             e.preventDefault();
+//         },
+//         false
+//     );
+// } else {
+//     // IE < 9
+//     document.attachEvent("oncontextmenu", function() {
+//         alert("You've tried to open context menu");
+//         window.event.returnValue = false;
+//     });
+// }
+
+//color swapping
+// function bgColor1() {
+//     // document.getElementById("body").style.backgroundColor = "#1ACCAB";
+//     // document.getElementById("body").style.color = "#e0e9fe";
+
+//     document
+//         .getElementById("body")
+//         .setAttribute(
+//             "style",
+//             "color:#1A3ACC !important;background-color:#1ACCAB !important;"
+//         );
+// }
+
+// function bgColor2() {
+//     // document.getElementById("body").style.backgroundColor = "#1A3ACC";
+//     document
+//         .getElementById("body")
+//         .setAttribute(
+//             "style",
+//             "color:white !important;background-color:#1A3ACC !important;"
+//         );
+// }
